f0a41c3192b8e34c1264b461c6eb172f02fbc3d1
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-14 12:19:16 -0500

    finished drill

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -96,7 +97,7 @@ app.post('/recipes', jsonParser, (req, res) => {
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
+      const message = `Missing \`${field}\` in request body`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -104,6 +105,34 @@ app.post('/recipes', jsonParser, (req, res) => {
   const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
+
+// call `ShoppingList.update` with updated item.
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name','ingredients', 'id']; //the order doesn't matter here; using the array for validation only 
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  
+  Recipes.update({
+    name: req.body.name,
+    id: req.params.id,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).end();
+});
+
 
 app.delete('/recipes/:id', (req, res) => {
   Recipes.delete(req.params.id);
